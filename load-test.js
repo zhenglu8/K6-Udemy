84a869fd6c8d48087ddf3df01751f17ce29e3345
Zheng Lu <zhenglu@Zhengs-MacBook-Pro.local>
2024-07-11 17:04:05 -0700

    load test

---
+++ load-test.js
@@ -0,0 +1,32 @@
+import http from 'k6/http';
+import {sleep} from 'k6';
+
+export const options = {
+    stages: [
+        {
+            // ramp up
+            duration: '5m',
+            target: 100
+        },
+        {
+            duration: '30m',
+            target: 100
+        },
+        {
+            // ramp down
+            duration: '5m',
+            target: 0
+        }
+    ]
+}
+
+export default function () {
+    http.get('https://test.k6.io');
+    sleep(1);
+    http.get('https://test.k6.io/contacts.php');
+    sleep(2);
+    http.get('https://test.k6.io/news.php');
+    sleep(2);
+}
+
+// Run load test every time before to production build
